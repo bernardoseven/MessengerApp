a8725d6cb5a2d374dcba49581a68794aceeed612
bernardoseven <bernardoseven@gmail.com>
2015-12-31 02:04:32 +0000

    lots of changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get '/messages' => 'messages#index'
+get 'messages/new' => 'messages#new'
+post 'messages' => 'messages#create'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
